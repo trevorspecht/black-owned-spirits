34f12e5378dbf8b1082443173b001660eaa40521
tree4 <tree4@tree4s-MacBook-Pro.local>
2021-01-01 19:43:48 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,51 @@
+
+const config = {
+    style: "mapbox://styles/mapbox/dark-v10",
+    accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrYzJoM2t4ODAxNDAycnF0cHo5eHoybDcifQ.-Pw1y6ZbWuUMooRWmJAK1Q",
+    CSV: "https://docs.google.com/spreadsheets/d/1BIqmPT06veFDmiBFL-80DtxIr44tVr8xoXUE2_x0TE0/edit?usp=sharing",
+    center: [-76.610759, 39.2908816], //Lng, Lat
+    zoom: 11, //Default zoom
+    title: "Black-owned Spirits",
+    description: "Find spirits available by location",
+    sideBarInfo: ["Name", "Address", "Website", "Phone"],
+    popupInfo: ["Name", "Website"],
+    filters: [
+        {
+            type: "dropdown",
+            title: "Spirit Type",
+            columnHeader: "Spirit Type",
+            listItems: [
+              'Vodka',
+              'Bourbon/Whiskey',
+              'Tequila',
+              'Rum',
+              'Gin',
+              'Cognac',
+              'Liqueur/Other',
+              'Multiple Types'
+            ]
+        }
+      /*  {
+            type: "checkbox",
+            title: "Title of filter: ",
+            columnHeader: "Column Name",
+            listItems: ["filter one", "filter two", "filter three"]
+        },
+        {
+            type: "dropdown",
+            title: "Title of filter: ",
+            columnHeader: "Column Name",
+            listItems: [
+                'filter one',
+                'filter two',
+                'filter three',
+                'filter four',
+                'filter five',
+                'filter six',
+                'filter seven'
+            ]
+        }
+        */
+    ]
+
+};
---
+++ scraper.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+import geocoder
+
+# get web page and parse html into soup object
+url = 'https://www.willdrinkfortravel.com/posts?category=Black-Owned%20Spirits'
+html = requests.get(url)
+soup = BeautifulSoup(html.text, "lxml")
+
+token = 'pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrYzJoM2t4ODAxNDAycnF0cHo5eHoybDcifQ.-Pw1y6ZbWuUMooRWmJAK1Q'
+
+# gets everything between the ()
+addressregex = re.compile(r'(?<=\().+(?=\))')
+# gets everything after the ()
+descregex = re.compile(r'(?<=\) ).+')
+
+with open('restaurants.csv', mode='w', newline='') as restaurants:
+  rest_writer = csv.writer(restaurants, delimiter=',', quotechar='"')
+  # iterates over <li> tags
+  for i, listing in enumerate(soup.find_all('li')):
+    # initialize variables for each iteration
+    text = ''
+    address = ''
+    description = ''
+    latitude = ''
+    longitude = ''
+    # parse name and web page from <a> tags
+    name = listing.a.string
+    link = listing.a.get('href')
+    # isolate text in <p> tags
+    if len(listing.p.contents) > 1:
+      text = str(listing.p.contents[1])
+    # isolate address
+    addressmatch = addressregex.search(text)
+    if addressmatch:
+      address = addressmatch.group(0)
+      # forward geocode map coordinates from address
+      geocoderesponse = geocoder.mapbox(address, key=token)
+      if geocoderesponse:
+        latitude = geocoderesponse.latlng[0]
+        longitude = geocoderesponse.latlng[1]
+    # isolate description
+    descmatch = descregex.search(text)
+    if descmatch:
+      description = descmatch.group(0)
+    # write to csv
+    rest_writer.writerow([name, address, latitude, longitude, link, description])
