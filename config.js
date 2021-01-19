86eb3e817bd07fe7603ac9407367c4df3beba22a
trevorspecht <trevor.specht@gmail.com>
2021-01-18 23:37:31 -0500

    update Mapbox token

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 const config = {
     style: "mapbox://styles/mapbox/dark-v10",
-    accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrYzJoM2t4ODAxNDAycnF0cHo5eHoybDcifQ.-Pw1y6ZbWuUMooRWmJAK1Q",
+    accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrazNoazVvaDBpZmMycGszNWZkYWl0Y3QifQ.d2ogRic-OLtqdvBk0YZltA",
     CSV: "https://docs.google.com/spreadsheets/d/1BIqmPT06veFDmiBFL-80DtxIr44tVr8xoXUE2_x0TE0/gviz/tq?tqx=out:csv&sheet=Sheet1",
     center: [-120.7790381, 51.7079466], //Lng, Lat
     zoom: 3, //Default zoom
