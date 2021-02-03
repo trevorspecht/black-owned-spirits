7d8bdc34f1a69b8d7f014aef03f99b086ee18984
trevorspecht <trevor.specht@gmail.com>
2021-02-03 13:21:52 -0500

    try zoom level 3

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrazNoazVvaDBpZmMycGszNWZkYWl0Y3QifQ.d2ogRic-OLtqdvBk0YZltA",
     CSV: "https://docs.google.com/spreadsheets/d/1BIqmPT06veFDmiBFL-80DtxIr44tVr8xoXUE2_x0TE0/gviz/tq?tqx=out:csv&sheet=Sheet1",
     center: [-100, 39], //Lng, Lat
-    zoom: 2, //Default zoom
+    zoom: 3, //Default zoom
     title: "Black-owned Spirits",
     description: "Click on a location to find available spirits",
     sideBarInfo: ["Name", "Spirit", "Locations", "Description"],
