433ea47d3cf7653c916b4d3871503ff6ab90bb52
trevorspecht <trevor.specht@gmail.com>
2021-02-03 13:16:59 -0500

    change center and zoom

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const config = {
     style: "mapbox://styles/mapbox/dark-v10",
     accessToken: "pk.eyJ1IjoidHJldm9yc3BlY2h0IiwiYSI6ImNrazNoazVvaDBpZmMycGszNWZkYWl0Y3QifQ.d2ogRic-OLtqdvBk0YZltA",
     CSV: "https://docs.google.com/spreadsheets/d/1BIqmPT06veFDmiBFL-80DtxIr44tVr8xoXUE2_x0TE0/gviz/tq?tqx=out:csv&sheet=Sheet1",
-    center: [-120.7790381, 51.7079466], //Lng, Lat
-    zoom: 3, //Default zoom
+    center: [-100, 39], //Lng, Lat
+    zoom: 4, //Default zoom
     title: "Black-owned Spirits",
     description: "Click on a location to find available spirits",
     sideBarInfo: ["Name", "Spirit", "Locations", "Description"],
