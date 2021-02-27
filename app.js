44b3d8dedec31f779b51a8b10312a5e8f072fd14
Trevor Uher Specht <trevor.specht@gmail.com>
2021-02-27 14:18:46 -0500

    add nationwide as a catchall for state clicks (#17)

--- app.js
+++ app.js
@@ -539,7 +539,7 @@ map.on("load", function () {
         const clickedState = e.features[0].properties.STATE_NAME;
         geojsonData.features.forEach(function (feature) {
             const locations = feature.properties.Locations;
-            if (locations.includes(clickedState))
+            if (locations.includes(clickedState || 'nationwide' || 'Nationwide'))
                 clickedStateLocations.features.push(feature);
         });
         map.getSource("locationData").setData(clickedStateLocations);
